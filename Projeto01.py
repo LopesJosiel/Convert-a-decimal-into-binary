22e3ff982d7d6b175a82c2e3eb96536d9bcdf96d
LopesJosiel <josielslopes@gmail.com>
2023-04-29 12:03:07 -0300

    Avançando no projeto

--- Projeto01.py
+++ Projeto01.py
@@ -4,14 +4,17 @@ def myfunction (x):
     resultado = x // 2 
     return resultado
 #Declarando a variável que vai receber o imput, já declarei como número
-dec = float (input('escolha um decimal para transformar em binário'))
-list_b = ()
+dec = input('escolha um decimal para transformar em binário' )
+dec = int (dec)
+
 myfunction(dec)
 while (myfunction (dec) % 2 != 0):
+    list_b = []
     list_b.append((myfunction (dec) % 2))
-    myfunction(dec)
+    myfunction(myfunction (dec)) #criando uma função autocorrelacionada
 #eu queria concatenar todos os elementos de lista para que eles aparecessem juntos no print
 print(list_b)
+
 #fazer com que a lista não apareça vazia, mas apresente todos os restos da divisão inteira por dois!
 
 
